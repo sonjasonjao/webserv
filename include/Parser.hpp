45eb0a2b7f6e07e29680578fdfcdad05385b6d41
Thiwanka Somachandra <thivankas@outlook.com>
2026-01-22 12:19:44 +0200

    change the varibale type to optional

--- include/Parser.hpp
+++ include/Parser.hpp
@@ -27,8 +27,8 @@ struct Config {
 	std::map<std::string, std::string>	status_pages;	// Mapping from HTTP status code to custom page path.
 	std::map<std::string, std::string>	routes;			// Set of routes (URI -> path definitions) for this server.
 
-	size_t	client_max_body_size	= CLIENT_MAX_BODY_SIZE;				// Default maximum allowed size (in bytes) of the request body for this server
-	std::string upload_dir			= "www/uploads";	// Default upload directory
+	std::optional<size_t> client_max_body_size;				// Default maximum allowed size (in bytes) of the request body for this server
+	std::optional<std::string> upload_dir;	// Default upload directory
 
 	bool	directoryListing	= false;
 	bool	autoindex			= false;
