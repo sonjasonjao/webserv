a366abea9e82b6c620c20ed2a57da69db719eaca
Johnny Varila <jvarila@student.hive.fi>
2026-02-03 14:43:56 +0200

    Added siege targets text file and some fixes to formatting.

--- include/CgiHandler.hpp
+++ include/CgiHandler.hpp
@@ -3,25 +3,24 @@
 #include "Request.hpp"
 #include <string>
 #include <map>
-#include <vector>
 #include <utility>
 #include <sys/types.h>
 
-struct CgiResponse
-{
-    std::string status = "200 OK";
-    std::string contentType = "text/plain";
-    std::string contentLength = "0";
-    std::string body;
+struct CgiResponse {
+	std::string	status			= "200 OK";
+	std::string	contentType		= "text/plain";
+	std::string	contentLength	= "0";
+	std::string	body;
 };
 
-class CgiHandler
-{
+class CgiHandler {
+	using StringMap = std::map<std::string, std::string>;
+
 public:
-    static std::pair<pid_t, int> execute(const std::string &scriptPath, const Request &request);
-    static CgiResponse parseCgiOutput(const std::string &rawOutput);
+	static std::pair<pid_t, int>	execute(std::string const &scriptPath, Request const &request);
+	static CgiResponse				parseCgiOutput(std::string const &rawOutput);
 
 private:
-    static std::map<std::string, std::string> getEnv(const std::string &scriptPath, const Request &request);
-    static char **mapToEnvp(const std::map<std::string, std::string> &envMap);
+	static StringMap	getEnv(std::string const &scriptPath, Request const &request);
+	static char			**mapToEnvp(const std::map<std::string, std::string> &envMap);
 };
