04ff04215147528ab25d3152679b9e8427f2154f
Johnny Varila <jvarila@student.hive.fi>
2026-02-11 14:21:06 +0200

    Removing extra files, preparing for evaluation.

--- www/site/cgi-bin/no_shebang.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-body = "Hello from CGI!\n"
-sys.stdout.write("Status: 200 OK\r\n")
-sys.stdout.write("Content-Type: text/plain\r\n")
-sys.stdout.write(f"Content-Length: {len(body)}\r\n\r\n")
-sys.stdout.write(body)
-sys.stdout.flush()
